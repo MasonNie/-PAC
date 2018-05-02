1b14295b04780c93c994c69bedc985a87e18b687
冠雄 聂 <743303584@qq.com>
2018-05-02 22:15:05 +0800

    判断文件是否存在

--- Graduate/src/analysis.java
+++ Graduate/src/analysis.java
@@ -1,7 +1,21 @@
+import java.io.File;
 
 public class analysis {
 	public analysis() {
 		
+	}
+	public Boolean isFileExists(String path){
+		File file=new File(path);
+		if(file.exists())
+		{
+			return true;
+		}
+		
+		else {
+			
+			return false;
+		}
+			
 	}
 
 }
